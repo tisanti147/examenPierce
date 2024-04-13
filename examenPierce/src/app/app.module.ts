281109b054970951cded864c4e73f59bdb9c982b
tisanti147 <santid147@gmail.com>
2024-04-13 19:38:23 -0300

    header y navbar terminados (desktop)

--- examenPierce/src/app/app.module.ts
+++ examenPierce/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { FooterComponent } from './components/shared/footer/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header/header.component';
+import { NavbarComponent } from './components/shared/navbar/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HomePageComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
