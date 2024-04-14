30d2f37e24b5570f016cb85025382924b6ce9c5d
tisanti147 <santid147@gmail.com>
2024-04-13 21:03:34 -0300

    slider, angular update

--- examenPierce/src/app/app.module.ts
+++ examenPierce/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -8,6 +7,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { FooterComponent } from './components/shared/footer/footer/footer.component';
 import { HeaderComponent } from './components/shared/header/header/header.component';
 import { NavbarComponent } from './components/shared/navbar/navbar/navbar.component';
+import { CarouselComponent, CarouselIndicatorsComponent, CarouselInnerComponent, CarouselItemComponent } from '@coreui/angular';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,11 @@ import { NavbarComponent } from './components/shared/navbar/navbar/navbar.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselComponent,
+    CarouselIndicatorsComponent,
+    CarouselInnerComponent,
+    CarouselItemComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
--- examenPierce/src/app/components/home/home.component.ts
+++ examenPierce/src/app/components/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.slides[0] = {
+      src: './assets/img/banner.jpeg',
+    };
+    this.slides[1] = {
+      src: './assets/img/futbol.jpeg',
+    };
+    this.slides[2] = {
+      src: './assets/img/zapatilla.jpeg',
+    };
+  }
 }
